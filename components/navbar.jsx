09c20469ee060c4fdef4beea222f3e46bc0a269e
Memet Zx <memetpea25@gmail.com>
2023-01-16 15:30:03 +0700

    menghapus warna putih di logo
    ini dikarenakan jika saat mode terang, logo tidak akan kelihatan
    jadi lebih baik di ikutkan saja denga text yang sudah ada di navbar.
    
    Signed-off-by: Memet Zx <memetpea25@gmail.com>

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -17,7 +17,7 @@ export default function Navbar() {
                 <Link href="/">
                   <a className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
                     <span>
-                      <GiCook color="white" size="24" />
+                      <GiCook  size="24" />
                       {/* <img
                         src="/img/logo.svg"
                         alt="N"
