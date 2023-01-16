00be35b83d5fe287e1d31ec1d0aec3d5739e720b
Memet Zx <memetpea25@gmail.com>
2023-01-16 15:31:12 +0700

    update sedikit `faq.jsx`
    mungkin ini adalah pertanyaan yang akan sering ditanyakan
    oleh pelanggan.
    Signed-off-by: Memet Zx <memetpea25@gmail.com>

--- components/faq.jsx
+++ components/faq.jsx
@@ -37,21 +37,15 @@ export default function Faq() {
 
 const faqdata = [
   {
-    question: 'Is this template completely free to use?',
-    answer: 'Yes, this template is completely free to use.',
+    question: 'Apakah ada biaya tambahan untuk layanan pesan antar?',
+    answer: 'Biaya tambahan untuk layanan pesan antar bervariasi tergantung pada jarak dan kondisi lalu lintas.',
   },
   {
-    question: 'Can I use it in a commercial project?',
-    answer: 'Yes, this you can.',
+    question: 'Bagaimana cara memesan makanan?',
+    answer: 'Anda dapat memesan makanan melalui telepon, atau facebook messenger.',
   },
   {
-    question: 'What is your refund policy? ',
-    answer:
-      "If you're unhappy with your purchase for any reason, email us within 90 days and we'll refund you in full, no questions asked.",
-  },
-  {
-    question: 'Do you offer technical support? ',
-    answer:
-      "No, we don't offer technical support for free downloads. Please purchase a support plan to get 6 months of support.",
+    question: 'Apakah ada batasan jarak untuk layanan pesan antar?',
+    answer: 'Ada batasan jarak untuk layanan pesan antar, karena warung UMKM hanya melayani pesanan di daerah terdekat.',
   },
 ];
